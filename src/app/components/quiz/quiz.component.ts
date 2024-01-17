28d26de4db9cdc62c3d372a52ea9d2b305e58218
danielnevesfonseca <daniel.neves.fonseca.ti@gmail.com>
2024-01-17 17:22:10 -0300

    feat: finish project and logics

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import quiz_questions_db from '../../../assets/data/quiz_questions_mock_db.json';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.scss']
+  styleUrls: ['./quiz.component.scss'],
 })
 export class QuizComponent implements OnInit {
+  title: string = 'Título Genérico';
+  finished: boolean = false;
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+  questions: any;
+  questionSelected: any;
+  answers: string[] = [];
+  answerSelected: string = '';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    if (quiz_questions_db) {
+      this.finished = false;
+      this.title = quiz_questions_db.title;
+
+      this.questions = quiz_questions_db.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
   }
 
+  playerChoice(value: string) {
+    this.answers.push(value);
+    this.nextStep();
+  }
+
+  async nextStep() {
+    this.questionIndex += 1;
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      const finalAnswer: string = await this.checkResult(this.answers);
+      this.finished = true;
+      this.answerSelected =
+        quiz_questions_db.results[
+          finalAnswer as keyof typeof quiz_questions_db.results
+        ];
+    }
+  }
+
+  async checkResult(answers: string[]) {
+    const result = answers.reduce((prev, current, i, arr) => {
+      if (
+        arr.filter((item) => item === prev).length >
+        arr.filter((item) => item === current).length
+      ) {
+        return prev;
+      } else {
+        return current;
+      }
+    });
+    return result;
+  }
 }
